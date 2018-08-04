51e44517c66549a4265af59b23ef2dd4b70ca711
jaztan <jaztangr@outlook.com>
2018-08-04 17:11:29 -0500

    eligiendo tipo de menu

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  let config = {
+    apiKey: "AIzaSyCTs2XUDLzl0m3e6Un5lNnEriz7krRaUfk",
+    authDomain: "mapa-de-comida.firebaseapp.com",
+    databaseURL: "https://mapa-de-comida.firebaseio.com",
+    projectId: "mapa-de-comida",
+    storageBucket: "mapa-de-comida.appspot.com",
+    messagingSenderId: "723749674184"
+  };
+  firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,38 @@ btnMenu.addEventListener('click', event => {
     let personx = prompt('Ingresa tu nombre', "Harry Potter");
     if (personx != null) {
         document.getElementById('saludo').innerHTML =
-        'Bienvenido ' + personx + '!¿Con hambre?¡'
+            'Bienvenidx ' + personx + '!¿Con hambre?¡'
     };
 });
+const btnLugar = document.getElementById('ubicacion');
+btnLugar.addEventListener('click', event => {
 
+    let lugar = prompt('Ingresa tu ubicacion', "Insurgentes Sur");
+    if (lugar != null) {
+        document.getElementById('lugar').innerHTML=
+            'Ubicacion ' + lugar + '!¿Que te gustaria comer hoy?¡'
+    };
+});
+var test = document.getElementById("precio");
+
+
+// this handler will be executed only once when the cursor moves over the unordered list
+test.addEventListener("mouseenter", function (event) {
+    // highlight the mouseenter target
+    event.target.style.color = "purple";
+
+    // reset the color after a short delay
+    setTimeout(function () {
+        event.target.style.color = "";
+    }, 500);
+}, false);
+// this handler will be executed every time the cursor is moved over a different list item
+test.addEventListener("mouseover", function (event) {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+
+    // reset the color after a short delay
+    setTimeout(function () {
+        event.target.style.color = "";
+    }, 500);
+}, false);
