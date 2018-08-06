868fa721612e545ed4a958f93f651da2cb5223a8
jaztan <jaztangr@outlook.com>
2018-08-06 16:02:32 -0500

    seleccionar menu

--- src/index.js
+++ src/index.js
@@ -16,26 +16,4 @@ btnLugar.addEventListener('click', event => {
             'Ubicacion ' + lugar + '!¿Que te gustaria comer hoy?¡'
     };
 });
-let test = document.getElementById("precio");
 
-
-// this handler will be executed only once when the cursor moves over the unordered list
-test.addEventListener("mouseenter", function (event) {
-    // highlight the mouseenter target
-    event.target.style.color = "purple";
-
-    // reset the color after a short delay
-    setTimeout(function () {
-        event.target.style.color = "";
-    }, 500);
-}, false);
-// this handler will be executed every time the cursor is moved over a different list item
-test.addEventListener("mouseover", function (event) {
-    // highlight the mouseover target
-    event.target.style.color = "orange";
-
-    // reset the color after a short delay
-    setTimeout(function () {
-        event.target.style.color = "";
-    }, 500);
-}, false);
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,11 @@
 const menu = document.getElementById('menu');
-btnMenu.addEventListener('onmouseover', event => {
-    let tacos = document.getElementById('tacos');
-    
+let miMenu = document.getElementById('mi-menu');
+miMenu.addEventListener('change', event=> {
+    console.log(event.target.value);
+    let tacos = document.getElementById('mi-menu').value;
+    let eleccion =document.getElementById('eleccion');
+    eleccion.innerHTML ="<p>Tu has elegido tacos de: "+tacos+"</p>";
+
+});
+
+   
